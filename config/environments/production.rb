4ce8e7a69dd851093afa0c067a36f68b9a1ba8be
Andre Luna <alm3@cin.ufpe.br>
2018-11-25 17:33:13 +0000

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
   end
   config.assets.compile = true
   config.action_cable.url = "wss://devincipe.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['*']
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
